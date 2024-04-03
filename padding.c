c1fd7e828a769e5ee42d5984551142690f7f9a62
DireRaven22075 <hanhs9803753@gmail.com>
2024-04-04 08:00:47 +0900

    Comment padding.c

--- padding.c
+++ padding.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+//구조체 사이즈 19바이트
 struct student {
     char lastName[13]; /* 13 bytes */
     int studentId; /* 4 bytes */
@@ -7,8 +8,15 @@ struct student {
 int main(void) {
     struct student pst;
     printf("[----- [한윤수] [2023041004] -----]\n");
+    //구조체 크기 출력 (24바이트)
     printf("size of student = %ld\n", sizeof(struct student));
+    //정수형 크기 출력 (4바이트)
     printf("size of int = %ld\n", sizeof(int));
+    //short형 크기 출력 (2바이트)
     printf("size of short = %ld\n", sizeof(short));
+
+    //구조체의 원할한 사용을 위해 padding을 통해 24바이트로 맞춤
+    //24바이트 = 8바이트 * 3
+    
     return 0;
 }
