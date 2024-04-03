c1e39c3a5e97c4055f8ef9971ed7434d1c74f884
DireRaven22075 <hanhs9803753@gmail.com>
2024-04-04 07:27:51 +0900

    Comment size.c

--- size.c
+++ size.c
@@ -2,10 +2,14 @@
 #include<stdlib.h>
 
 int main(void) {
+    //이중 포인터 변수 x 선언
     int* *x;
     printf("[----- [한윤수] [2023041004] -----]\n");
+    //이중 포인터 변수 x의 사이즈 출력 (8바이트)
     printf("sizeof(x) = %lu\n", sizeof(x));
+    //이중 포인터 변수 x의 1차 참조의 사이즈 출력 (8바이트)
     printf("sizeof(*x) = %lu\n", sizeof(*x));
+    //이중 포인터 변수 x의 2차 참조의 사이즈 출력 (4바이트)
     printf("sizeof(**x) = %lu\n", sizeof(**x));
     return 0;
 }
